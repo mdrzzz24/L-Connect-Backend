42a54e75cd6bd0748394b7376e58c9d50c581262
mdrzzz24 <achmadrozan24@gmail.com>
2023-10-20 12:30:56 +0700

    add update user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,63 +1,103 @@
-<?php
+    <?php
 
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Hash;
+    use App\Models\User;
+    use Illuminate\Http\Request;
+    use Illuminate\Http\Response;
+    use Illuminate\Support\Facades\Hash;
 
-class AuthController extends Controller
-{
-    public function Register(Request $request) {
-        $request->validate([
-            "name" => "required|string",
-            "phone" => "required|string",
-            "email" => "required|email|unique:users,email",
-            "password" => "required|string|min:6",
-        ]);
+    class AuthController extends Controller
+    {
+        public function Register(Request $request) {
+            $request->validate([
+                "name" => "required|string",
+                "phone" => "required|string",
+                "email" => "required|email|unique:users,email",
+                "password" => "required|string|min:6",
+            ]);
 
-        $user = User::create([
-            "name" => $request->name,
-            "phone" => $request->phone,
-            "email" => $request->email,
-            "password" => Hash::make($request->password),
-        ]);
+            $user = User::create([
+                "name" => $request->name,
+                "phone" => $request->phone,
+                "email" => $request->email,
+                "password" => Hash::make($request->password),
+            ]);
 
-        $token = $user->createToken("Auth_Token")->plainTextToken;
+            $token = $user->createToken("Auth_Token")->plainTextToken;
 
-        return response()->json([
-            "message" => "Registration successful",
-            "token" => $token
-        ], Response::HTTP_CREATED);
-    }
-
-    function Login(Request $request) {
-        $request->validate([
-            "email" => "required|string",
-            "password" => "required|string",
-        ]);
-
-        $user = User::firstWhere("email", $request->email);
-
-        if(!$user || !Hash::check($request->password, $user->password)) {
             return response()->json([
-                "message" => "Bad Credentials!"
-            ], Response::HTTP_NOT_FOUND);
+                "message" => "Registration successful",
+                "token" => $token
+            ], Response::HTTP_CREATED);
+        }
+        function Login(Request $request) {
+            $request->validate([
+                "email" => "required|string",
+                "password" => "required|string",
+            ]);
+
+            $user = User::firstWhere("email", $request->email);
+
+            if(!$user || !Hash::check($request->password, $user->password)) {
+                return response()->json([
+                    "message" => "Bad Credentials!"
+                ], Response::HTTP_NOT_FOUND);
+            }
+
+            $token = $user->createToken("Auth_Token")->plainTextToken;
+
+            return response()->json([
+                "message" => "Success Login",
+                "token" => $token
+            ], Response::HTTP_OK);
+        }
+        public function getUserInfo() {
+            $user = auth()->user();
+            // dd($user);
+
+            return response()->json([
+                "user" => $user
+            ], Response::HTTP_OK);
+        }
+        public function updateUser(Request $request) {
+            $user = auth()->user(); // Get the currently authenticated user
+
+            $request->validate([
+                "name" => "string", // Name should be a string
+                "phone" => "string", // Phone should be a string
+                "email" => "email|unique:users,email," . $user->id, // Email should be a valid email format and unique in the 'users' table, except for the current user
+                "password" => "string|min:6", // Password should be a string with a minimum length of 6 characters
+            ]);
+
+            // Update user data based on the request
+            if ($request->has('name')) {
+                $user->name = $request->name;
+            }
+            if ($request->has('phone')) {
+                $user->phone = $request->phone;
+            }
+            if ($request->has('email')) {
+                $user->email = $request->email;
+            }
+            if ($request->has('password')) {
+                $user->password = Hash::make($request->password); // Hash the password before saving it
+            }
+
+            // Save the changes to the user
+            $user->save();
+
+            return response()->json([
+                "message" => "User information updated successfully",
+                "user" => $user
+            ], Response::HTTP_OK);
         }
 
-        $token = $user->createToken("Auth_Token")->plainTextToken;
+        function Logout() {
+            auth()->user()->tokens()->delete();
 
-        return response()->json([
-            "message" => "Success Login",
-            "token" => $token
-        ], Response::HTTP_OK);
+            return response()->json([
+                "message" => "Success Logout"
+            ], Response::HTTP_OK);
+        }
     }
-    function Logout() {
-        auth()->user()->tokens()->delete();
-
-        return response()->json([
-            "message" => "Success Logout"
-        ], Response::HTTP_OK);
-    }
-}
--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::post("/auth/login", [AuthController::class, "Login"]);
 Route::post("/auth/register", [AuthController::class, "Register"]);
 Route::middleware('auth:sanctum')->post("/auth/logout", [AuthController::class, "Logout"]);
-
+Route::middleware('auth:sanctum')->get("/auth/info", [AuthController::class, "getUserInfo"]);
+Route::middleware('auth:sanctum')->put("/auth/update", [AuthController::class, "updateUser"]);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
